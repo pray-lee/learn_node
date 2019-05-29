cc0d565609fe9362c0dc094d1c1e4d7c899ab1c4
pray-lee <praycis@gmail.com>
2019-05-29 17:53:43 +0800

    update

---
+++ koa2-demo/blog-koa2/app.js
@@ -0,0 +1,51 @@
+const Koa = require('koa')
+const app = new Koa()
+const json = require('koa-json')
+const onerror = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+const redisStore = require('koa-redis')
+const session = require('koa-session')
+
+const index = require('./routes/index')
+const user = require('./routes/user')
+const blog = require('./routes/blog')
+
+// error handler
+onerror(app)
+
+// middlewares
+app.use(bodyparser({
+  enableTypes:['json', 'form', 'text']
+}))
+// app.use(json())
+app.use(logger())
+app.keys=["Wj_67*"]
+app.use(session({
+  key: 'koa:sess',
+  maxAge: 24*60*60*1000,
+  httpOnly:true,
+  store: redisStore({
+    all: 'http://localhost:6379'
+  })
+}, app))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+app.use(user.routes(), user.allowedMethods())
+app.use(blog.routes(), blog.allowedMethods())
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx)
+});
+
+module.exports = app
---
+++ koa2-demo/blog-koa2/routes/blog.js
@@ -0,0 +1,29 @@
+const router = require('koa-router')()
+
+router.prefix('/api/blog')
+
+router.get('/list', async (ctx, next) => {
+  ctx.body = {
+    errno: 0,
+    list: [
+      1,2,3
+    ]
+  }
+})
+
+router.get('/detail', async (ctx, next) => {
+  console.log('------------------------------------------------------/n')
+  console.log(ctx.request.header)
+  console.log('==============')
+  console.log(ctx.response.headers)
+  const id = ctx.request.id
+  ctx.body = {
+    errno: 0,
+    data: {
+      a: 1,
+      b: 2
+    }
+  }
+})
+
+module.exports = router
